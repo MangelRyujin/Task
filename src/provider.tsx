92c78aed13ed550169f0894249ac5623aa0ae2c9
mangelryujin <mangelryujin@gmail.com>
2025-09-25 12:40:49 -0400

    remove default theme

--- src/provider.tsx
+++ src/provider.tsx
@@ -13,7 +13,7 @@ export function Provider({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
 
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light">
+    <NextThemesProvider>
       <HeroUIProvider 
         navigate={navigate} 
         useHref={useHref} 
